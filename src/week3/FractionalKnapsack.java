a1e70083ab09820c1ac566d6053fd19a00abe09f
barrida <suleyman.yildirim@accenture.com>
2019-02-28 11:45:57 +0000

    FractionalKnapsack

--- src/week3/FractionalKnapsack.java
+++ src/week3/FractionalKnapsack.java
@@ -8,11 +8,11 @@ import java.util.Scanner;
  * The goal of this code problem is to implement an algorithm for the fractional
  * knapsack problem
  * 
- * Calculate ratio of each item (v[i]/w[i]) 
- * (Optional) Sort the ratios in descending order
- * Choose the item with highest ratio
- * If it fits knapsack, take all value of it Otherwise take so much as to fill the knapsack Return total
- * value and amounts taken
+ * 1. Calculate ratio of each item (v[i]/w[i]) 
+ * 2. Sort the ratios in descending order
+ * 3. Choose the item with highest ratio
+ * 4. If it fits knapsack, take all value of it Otherwise take so much as to fill the knapsack
+ * 5. Return total value and amounts taken
  * 
  * @author suleyman.yildirim
  *
@@ -20,8 +20,9 @@ import java.util.Scanner;
 public class FractionalKnapsack {
 	
 	// item class 
-    static class Item { 
-        Integer value;
+    public static class Item { 
+
+		Integer value;
         Integer weight;
         Double ratio;
           
@@ -31,18 +32,44 @@ public class FractionalKnapsack {
             this.value = value; 
             this.ratio = ratio; 
         } 
+        
+        public Integer getValue() {
+			return value;
+		}
+
+		public void setValue(Integer value) {
+			this.value = value;
+		}
+
+		public Integer getWeight() {
+			return weight;
+		}
+
+		public void setWeight(Integer weight) {
+			this.weight = weight;
+		}
+
+		public Double getRatio() {
+			return ratio;
+		}
+
+		public void setRatio(Double ratio) {
+			this.ratio = ratio;
+		}
+
   
     } 
 
 	/**
-	 * Choose the item with highest ratio. If it fits knapsack, take all value
-	 * of it. Otherwise take so much as to fill the knapsack Return total value
-	 * and amounts taken
+	 * Choose the item with highest ratio. As Item class is sorted by ratio
+	 * descended, we iterate through array of Item objects. If it fits knapsack,
+	 * take all value of it and continiue with the next item. Otherwise take so
+	 * much as to fill the knapsack.
 	 * 
 	 * @param capacity
 	 * @param values
 	 * @param weights
-	 * @return
+	 * @return Return total value
 	 */
 	public static double getOptimalValue(int capacity, int[] values, int[] weights) {
 		double result = 0;
@@ -53,12 +80,8 @@ public class FractionalKnapsack {
 		sortRatio(ratios);
 		
 		for (int i = 0; i < ratios.length; i++) {
-
 			weight = ratios[i].weight;
 			value = ratios[i].value;
-//			System.out.println("value: " + value);
-//			System.out.println("weight: " + weight);
-//			System.out.println("ratio " + ratios[i].ratio);
 			if (capacity == 0)
 				return result;
 
@@ -71,17 +94,15 @@ public class FractionalKnapsack {
 			}
 		}
 		
-		
-		//System.out.println(result);
 		return Double.parseDouble(String.format("%.4f", result));
 	}
 
 	/**
-	 * (Optional) Sort the ratios in descending order
-	 * @param ratios
+	 * Sort the ratios in descending order
+	 * @param ratio
 	 */
-	private static void sortRatio(Item[] ratios) {
-        Arrays.sort(ratios, new Comparator<Item>() { 
+	public static void sortRatio(Item[] ratio) {
+        Arrays.sort(ratio, new Comparator<Item>() { 
             @Override
             public int compare(Item o1, Item o2) { 
                 return o2.ratio.compareTo(o1.ratio); 
@@ -104,39 +125,6 @@ public class FractionalKnapsack {
 		}
 		return items;
 	}
-	
-	/**
-	 * 
-	 * @param values
-	 * @param weights
-	 * @return
-	 */
-	// public static Map<Double, Integer[][]> calculateRatio(int[] values, int[]
-	// weights) {
-	//
-	// Integer[][] item = new Integer[values.length][2];
-	// // Map<Double,Integer[][]> ratioMap = new HashMap<Double,Integer[][]>();
-	//
-	// Map<Double, Integer[][]> ratioMap = new TreeMap<Double,
-	// Integer[][]>().descendingMap();
-	//
-	// for (int i = 0; i < weights.length; i++) {
-	// item[i][0] = values[i];
-	// item[i][1] = weights[i];
-	// //System.out.println(values[i] + " : " + weights[i]);
-	// // System.out.println( values[i] / weights[i] );
-	// ratioMap.put(Double.valueOf((double) (values[i] / weights[i])), item);
-	// }
-	//
-	// // Map<Double,Integer[][]> treeMap = new
-	// // TreeMap<>(ratioMap).descendingMap();
-	//
-	//// for (Double str : ratioMap.keySet()) {
-	//// System.out.println("ker: " + str);
-	//// }
-	//
-	// return ratioMap;
-	// }
 
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
--- test/week3/FractionalKnapsackTest.java
+++ test/week3/FractionalKnapsackTest.java
@@ -4,14 +4,9 @@
 package test.week3;
 
 import static org.junit.Assert.*;
-
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
 import org.junit.Test;
-
 import src.week3.FractionalKnapsack;
+import src.week3.FractionalKnapsack.Item;
 
 /**
  * @author suleyman.yildirim
@@ -22,17 +17,16 @@ public class FractionalKnapsackTest {
 	/**
 	 * Test method for {@link src.week3.FractionalKnapsack#getOptimalValue(int, int[], int[])}.
 	 */
-	@SuppressWarnings("deprecation")
 	@Test
 	public void testGetOptimalValue() {
+		
 		int[] values = {60, 100, 120};
-		int[] weights = {20, 50 ,30};
-		double value = FractionalKnapsack.getOptimalValue(50, values, weights);
-		System.out.println(value);
-		assertEquals(180.0000, value, 0.0000);
-		double val2 = FractionalKnapsack.getOptimalValue(10, new int[] {500}, new int [] {30});
-		System.out.println(val2);
-		assertEquals(166.6667, val2, 0.0000);
+		int[] weights = {20, 50 ,30};		
+		assertEquals(180.0000, FractionalKnapsack.getOptimalValue(50, values, weights), 0.0000);
+
+		int[] values2 = new int[] {500};
+		int[] weights2 = new int [] {30};
+		assertEquals(166.6667, FractionalKnapsack.getOptimalValue(10, values2, weights2), 0.0000);
 	}
 
 	/**
@@ -42,10 +36,27 @@ public class FractionalKnapsackTest {
 	public void testCalculateRatio() {
 		int[] values = {60, 100, 120};
 		int[] weights = {20, 50 ,30};
-		//Map<Double, Integer[][]> calculateRatio = FractionalKnapsack.calculateRatio(values, weights);
-		//assertNotNull(calculateRatio);
+		Item[] ratio = FractionalKnapsack.calculateRatio(values, weights);
 		
-
+		assertNotNull(ratio);
+		assertEquals(3,ratio[0].getRatio().intValue());
+		assertEquals(2,ratio[1].getRatio().intValue());
+		assertEquals(4,ratio[2].getRatio().intValue());
+	}
+	
+	/**
+	 * Test method for {@link src.week3.FractionalKnapsack#sortRatio(Item[])}.
+	 */
+	@Test
+	public void testSortRatio(){
+		int[] values = {60, 100, 120};
+		int[] weights = {20, 50 ,30};
+		Item[] ratio = FractionalKnapsack.calculateRatio(values, weights);
+		FractionalKnapsack.sortRatio(ratio);
+		
+		assertEquals(4,ratio[0].getRatio().intValue());
+		assertEquals(3,ratio[1].getRatio().intValue());
+		assertEquals(2,ratio[2].getRatio().intValue());
 	}
 
 }
